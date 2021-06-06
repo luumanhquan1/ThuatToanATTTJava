656e1f3202cc75ba780b8c346d1d3c942b339204
lưu mạnh quân <luumanhquan2001@gmail.com>
2021-06-06 16:56:52 +0700

    done

--- src/main.java
+++ src/main.java
@@ -3,22 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package multiplication;
-
-import plus_on_Fp.PlusOnFp;
 
 /**
  *
  * @author ASUS TUF
  */
-public class Multiplication extends PlusOnFp {
-
-    @Override
-    public void inPut() {
-        super.inPut();
-    }
-
-    public void multiply() {
-          
-    }
+public class main {
+    
 }
